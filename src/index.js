4f8997244f6c4a0aac9c66bdbf52004176ae266a
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-05-18 19:15:16 +0200

    added id's

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 let apiKey="751305c75f9526727cdf4f36e45a4e75";
 let city= "Valencia";
 let apiUrl=`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
- console.log(apiUrl);
+
+
+function getTemperature(){
+    
+}
+get(apiUrl).then(getTemperature);
+ 
