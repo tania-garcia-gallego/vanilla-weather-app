9919f2107fce1356ddc51b0f41dcd1d5ca057c85
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-05-18 19:37:03 +0200

    Javascript ul (temp, wind speed, etc)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 let apiKey="751305c75f9526727cdf4f36e45a4e75";
 let city= "Valencia";
-let apiUrl=`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
 
-
-function getTemperature(){
-    
+function getTemperature(response){
+let temperature = Math.round(response.data.main.temp);
+let currentTemp = document.querySelector("#temp");
+currentTemp.innerHTML = `${temperature} °C`;
+let humidity = Math.round(response.data.main.humidity);
+let currentHumidity = document.querySelector("#humidity");
+currentHumidity.innerHTML = `Humidity ${humidity}%`
+let windSpeed = Math.round(response.data.wind.speed);
+let currentSpeed = document.querySelector("#wind");
+currentSpeed.innerHTML = `Wind Speed: ${windSpeed}km/h`;
+let tempMax = Math.round(response.data.main.temp_max);
+let currentTempMax = document.querySelector("#tempMax");
+currentTempMax.innerHTML = `Temp Max ${tempMax}°C`;
+let tempMin = Math.round(response.data.main.temp_min);
+let currentTempMin = document.querySelector("#tempMin");
+currentTempMin.innerHTML = `Temp Min ${tempMin}°C`;
 }
-get(apiUrl).then(getTemperature);
+axios.get(apiUrl).then(getTemperature);
  
