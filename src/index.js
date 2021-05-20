c09e771717fb79818175cdb8dc3ad9218bb435f9
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-05-20 10:44:14 +0200

    Changed Javascript
    So the app takes directly the location of the user and displays automatically the weather in that position

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-let apiKey="751305c75f9526727cdf4f36e45a4e75";
-let city= "Valencia";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 
 function getTemperature(response){
-let temperature = Math.round(response.data.main.temp);
-let currentTemp = document.querySelector("#temp");
-currentTemp.innerHTML = `${temperature} °C`;
-let humidity = Math.round(response.data.main.humidity);
-let currentHumidity = document.querySelector("#humidity");
-currentHumidity.innerHTML = `Humidity ${humidity}%`
-let windSpeed = Math.round(response.data.wind.speed);
-let currentSpeed = document.querySelector("#wind");
-currentSpeed.innerHTML = `Wind Speed: ${windSpeed}km/h`;
-let tempMax = Math.round(response.data.main.temp_max);
-let currentTempMax = document.querySelector("#tempMax");
-currentTempMax.innerHTML = `Temp Max ${tempMax}°C`;
-let tempMin = Math.round(response.data.main.temp_min);
-let currentTempMin = document.querySelector("#tempMin");
-currentTempMin.innerHTML = `Temp Min ${tempMin}°C`;
+    let temp = Math.round(response.data.main.temp);
+    let currentTemp = document.querySelector("#temp");
+    currentTemp.innerHTML = `${temp} °C`;
+    let cityName = response.data.name;
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = cityName;
+    let humidity = Math.round(response.data.main.humidity);
+    let currentHum = document.querySelector("#humidity");
+    currentHum.innerHTML = `Humidity: ${humidity}%`;
+    let windSpeed = response.data.wind.speed;
+    let currentWind = document.querySelector("#wind");
+    currentWind.innerHTML = `Wind Speed: ${windSpeed} km/h`;
+    let maxTemp = Math.round(response.data.main.temp_max);
+    let todayMaxTemp = document.querySelector("#tempMax");
+    todayMaxTemp.innerHTML = `Max Temp: ${maxTemp}°C`;
+    let minTemp = Math.round(response.data.main.temp_min);
+    let todayMinTemp = document.querySelector("#tempMin");
+    todayMinTemp.innerHTML = `Min Temp: ${minTemp}°C`;
 }
-axios.get(apiUrl).then(getTemperature);
- 
+
+function getCity(response){
+    let city = response.data.name;
+    let apiKey = "751305c75f9526727cdf4f36e45a4e75";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getTemperature);
+
+}
+
+function getPosition(position){
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let apiKey = "751305c75f9526727cdf4f36e45a4e75";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getCity);
+}
+
+navigator.geolocation.getCurrentPosition(getPosition);
